30f174015cde93fa1de95ddcc8937bbb06f641d8
Arpit sharma <60608608+Arpitsh018@users.noreply.github.com>
2020-02-04 21:36:31 +0530

    Update FetchDataWithAes .php

--- FetchDataWithAes .php
+++ FetchDataWithAes .php
@@ -5,27 +5,9 @@
 	*@des It return Available if Data alredy exixt
 	*@Author Arpit sharma
 	*/
-
-		// Not show any error
-		error_reporting(0);
-		// Get server port type (exampale - http:// or https://)
-		if ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || $_SERVER['SERVER_PORT'] == 443) {
-			$HeaderSecureType = "https://";
-		}else{
-			$HeaderSecureType = "http://";
-		}
-		// Create Domain name and save it in const variable
-		define("DomainName",  $HeaderSecureType.$_SERVER['HTTP_HOST']."/Digital_gatepass");
-
-		if(!isset($FileAccessCode) || $FileAccessCode !== 'Granted_Permisition_w65!4GsD6t*P_B&BMX2U_E9ZUuRH94GvN_xNbUH' ){
-			header("Location: " . DomainName . "/PageNotAvailable/index.php");
-			die();
-			exit();
-		}
-
 		function FetchDataWithAes($GivenData,$Conditions,$DatabaseConnection,$DbTableName,$EncodeAndEncryptPass,$CheckFor = 'all'){
 			if(strlen($GivenData) == 0){
-				return ["status"=>"Error","msg"=>"Process failed! Try again later"];
+				return ["status"=>"Error","msg"=>"Given data is not found"];
 				exit();
 			}
 			
@@ -40,7 +22,7 @@
 				if(strpos($value, "::::") !== false){
 				   $TmpGivenDataArray = explode("::::",$value);
 				} else{
-				   return ["status"=>"Error","msg"=>"Process failed! Try again later","code"=>400]; exit();
+				   return ["status"=>"Error","msg"=>"Given data in invalid formate","code"=>400]; exit();
 				}
 				
 				if(preg_replace("/[^A-Za-z0-9_]/","",$TmpGivenDataArray[0]) !== ""){
@@ -54,7 +36,7 @@
 						array_push($StmtGivenDataVal, $TmpGivenDataArray[1]);
 					}
 				}else{
-					return ["status"=>"Error","msg"=>"Process failed! Try again later","code"=>400];
+					return ["status"=>"Error","msg"=>"Given data key (".$TmpGivenDataArray[0].") contaion invaild character","code"=>400];
 				}
 			}
 
@@ -68,7 +50,7 @@
 				if(strpos($value, "::::") !== false){
 				   $TmpGivenDataArray = explode("::::",$value);
 				} else{
-				   return ["status"=>"Error","msg"=>"Process failed! Try again later","code"=>400];
+				   return ["status"=>"Error","msg"=>"Conditions in invalid formate","code"=>400];
 				}
 
 				if(preg_replace("/[^A-Za-z0-9_]/","",$TmpGivenDataArray[0]) !== ""){
@@ -78,7 +60,7 @@
 						}else if($CheckFor === 'any'){
 							$TempConnector = '||';
 						}else{
-							return ["status"=>"Error","msg"=>"Process failed! Try again later","code"=>400]; exit();
+							return ["status"=>"Error","msg"=>"Invalid CheckFor detect","code"=>400]; exit();
 						}
 						if($i === 1){
 							$UpdateCondition = $TmpGivenDataArray[0] .' = AES_ENCRYPT(:'.$TmpGivenDataArray[0].'Cnd, :EncodeAndEncryptPass)';
@@ -89,39 +71,28 @@
 						array_push($ConditionsDataVal, $TmpGivenDataArray[1]);
 					}
 				}else{
-					return ["status"=>"Error","msg"=>"Process failed! Try again later","code"=>400];
+					return ["status"=>"Error","msg"=>"Condtion key (".$TmpGivenDataArray[0].") contaion invaild character","code"=>400];
 				}
 			}
-			if(isset($DatabaseConnection) && isset($DbTableName)){
+			
+			$stmt = $DatabaseConnection->prepare("UPDATE $DbTableName SET $UpdateString  WHERE $UpdateCondition");
 
-				// Check and remove user if account created but Status is pending
-				$stmt = $DatabaseConnection->prepare("UPDATE $DbTableName SET $UpdateString  WHERE $UpdateCondition");
-
-				$stmt->bindValue(":EncodeAndEncryptPass", $EncodeAndEncryptPass, PDO::PARAM_STR);
-				$i = 0;
-				foreach ($StmtGivenDataVal as $value) {
-					$stmt->bindValue(":".$StmtGivenDataKey[$i] , $value, PDO::PARAM_STR);
-					$i++;
-				}
-				$i = 0;
-				foreach ($ConditionsDataVal as $value) {
-					$stmt->bindValue(":".$ConditionsDataKey[$i] , $value, PDO::PARAM_STR);
-					$i++;
-				}
-				$stmt->execute();
-				if($stmt->rowCount() > 0){
-					return ["status"=>"Success","msg"=>'Data Update Successfully',"code"=>200];
-				}else{
-					return ["status"=>"Error","msg"=>"Data Not Update",'reason'=>json_encode($stmt->errorinfo()),"code"=>404];
-				}
+			$stmt->bindValue(":EncodeAndEncryptPass", $EncodeAndEncryptPass, PDO::PARAM_STR);
+			$i = 0;
+			foreach ($StmtGivenDataVal as $value) {
+				$stmt->bindValue(":".$StmtGivenDataKey[$i] , $value, PDO::PARAM_STR);
+				$i++;
+			}
+			$i = 0;
+			foreach ($ConditionsDataVal as $value) {
+				$stmt->bindValue(":".$ConditionsDataKey[$i] , $value, PDO::PARAM_STR);
+				$i++;
+			}
+			$stmt->execute();
+			if($stmt->rowCount() > 0){
+				return ["status"=>"Success","msg"=>'Data Updated Successfully',"code"=>200];
 			}else{
-				return ["status"=>"Error","msg"=>"Process failed! Try again later","code"=>400];
+				return ["status"=>"Error","msg"=>"Data Updation failed",'reason'=>json_encode($stmt->errorinfo()),"code"=>404];
 			}
 		}
 ?>
-<!--
-	Use ->
-	$Response = InsertGivenData($GivenData,$DatabaseConnection,$DbTableName,$EncodeAndEncryptPass);
-	Check Response -> 
-	if($Response['status'] === 'Success' && $Response['code'] === 200){}
--->
